97f8c483f1a796d6ea74e663782156a0a334f5cb
Miras <91594617+laqube@users.noreply.github.com>
2023-04-16 10:09:44 +0600

    feat | landing
    landing page ready, a whole lot to go

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,10 +1,86 @@
 //HEADER FOR UNIVERSAL USE
-import React from 'react'
+import * as React from 'react';
+import { AppBar, Toolbar, IconButton, Button, Grid, Container } from '@mui/material';
+import PublicIcon from '@mui/icons-material/Public';
+import { Link } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material/styles';
 
-const Header = () => {
+const theme = createTheme({
+  typography: {
+    button: {
+      fontSize: '1.05rem',
+      fontFamily: 'Raleway, sans-serif',
+      fontWeight: '500',
+    },
+  },
+  palette: {
+    primary: {
+      light: '#6fbf73',
+      main: '#fff',
+      dark: '#357a38',
+      contrastText: '#4caf50',
+    },
+    secondary: {
+      light: '#6fbf73',
+      main: '#4caf50',
+      dark: '#357a38',
+      contrastText: '#fff',
+    },
+  },
+});
+
+function Header() {
+      const [anchorElNav, setAnchorElNav] = React.useState(null);
+      const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+      const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+      };
+      const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+      };
+
+      const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+      };
+
+      const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
   return (
-    <div>header</div>
-  )
+    <ThemeProvider theme={theme}>
+      <AppBar position="static" variant='outlined'>
+        <Container maxWidth="lg" sx={{ mx: '5' }}>
+          <Toolbar disableGutters>
+            <Grid container alignItems="center" justifyContent="space-between">
+              <Grid item>
+                <IconButton color="inherit">
+                  <PublicIcon fontSize='large'/>
+                </IconButton>
+                <Link to="/">
+                  <Button variant="text" color="secondary" sx={{ mr: 2 }}>Главная</Button>
+                </Link>
+                <Link to="/">
+                  <Button variant="text" color="secondary" sx={{ mr: 1 }}>О Нас</Button>
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link to="/login">
+                  <Button variant="contained" color="secondary" sx={{ ml: 1 }}>Вход</Button>
+                </Link>
+                <Link to="/client/registration">
+                  <Button variant="contained" color="secondary" sx={{ ml: 2 }}>Регистрация</Button>
+                </Link>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      
+    </ThemeProvider>
+
+  );
 }
 
-export default Header
+export default Header;
--- client/src/pages/landing.jsx
+++ client/src/pages/landing.jsx
@@ -1,13 +1,126 @@
 //ЛЕНДИНГ СО ВХОДОМ
 import React from "react";
 import Header from "../components/header"
+import { Grid, Container, Box, Card, CardMedia, CardContent, Typography } from "@mui/material";
+import backgroundImage from './rsc/landingbg1.jpg';
+import pic1 from './rsc/landingpic1.jpg'
+import serviceImage1 from './rsc/service1.png';
+import serviceImage2 from './rsc/service2.png';
+import serviceImage3 from './rsc/service3.png';
+
+import {Button} from "@mui/material";
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material/styles'; 
+
+const styles = {
+  container: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '50vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'end',
+    color: 'white',
+  },
+};
+
+const theme = createTheme({
+  Box:{
+    justifyContent: 'end',
+  },
+  palette: {
+    primary: {
+      light: '#6fbf73',
+      main: '#fff',
+      dark: '#357a38',
+      contrastText: '#4caf50',
+    },
+    secondary: {
+      light: '#6fbf73',
+      main: '#4caf50',
+      dark: '#357a38',
+      contrastText: '#fff',
+    },
+    warning:{
+      main: '#E5C34D',
+      contrastText: '#000'
+    }
+  },
+});
+
+const services = [
+  {
+    title: 'Установка Экобокса',
+    
+    image: serviceImage1,
+  },
+  {
+    title: 'Вывоз Мусора',
+    
+    image: serviceImage2,
+  },
+  {
+    title: 'Демонтаж Экобокса',
+    
+    image: serviceImage3,
+  },
+];
 
 function Landing() {
+  
     return (
       <div>
         <Header/>
-        <h2>Welcome to my App!</h2>
-        {/* Home page content */}
+        <ThemeProvider theme={theme}>
+          <Box style={styles.container}>
+            <div style={{marginRight: '12%'}}>
+              <h3>Разделяй и Властвуй</h3>
+              <h2>Сервисы для защиты окружающей среды</h2>
+              <Button color="warning" variant="contained">Узнать больше</Button>
+            </div>
+          </Box>
+          <Container fixed style={{marginTop: '5%'}}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <img src={pic1} alt="mountains" />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <h1>Наша миссия - создать чистую среду!</h1>
+                <h3>Давно выяснено, что при оценке дизайна и композиции читаемый текст мешает сосредоточиться. </h3>
+              </Grid>
+            </Grid>
+          </Container>
+          <Container fixed style={{marginTop: '5%'}}>
+            <Grid item xs={12} style={{ textAlign: 'center', color: 'green' }}>
+              <Typography variant="h4" style={{margin: "10%"}}>
+                Наши Сервисы
+              </Typography>
+            </Grid>
+            <Grid container spacing={3}>
+              {services.map((service) => (
+                <Grid item key={service.title} xs={12} sm={6} md={4}>
+                  <Card style={{ borderRadius: 16 }}>
+                    <CardMedia
+                      style={{ height: 0, paddingTop: '100%' }}
+                      image={service.image}
+                      title={service.title}
+                    />
+                    <CardContent style={{ backgroundColor: '#fafafa', padding: '24px', textAlign: 'center',  }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {service.title}
+                      </Typography>
+                      <Typography variant="body2" color="textSecondary" component="p" style={{ backgroundColor: 'white', borderRadius: 8, padding: 8 }}>
+                        {service.description}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))}
+
+            </Grid>
+          </Container>
+        </ThemeProvider>
       </div>
     );
   }
